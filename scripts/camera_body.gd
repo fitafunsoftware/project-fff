092f93f1c8d505c42bcfe2ee3f1edc369aa201d6
Tareq Anuar <MTareqAzim@gmail.com>
2023-02-26 01:06:04 +0800

    Fix camera size to maintain pixels.

--- scripts/camera_body.gd
+++ scripts/camera_body.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED : float = 5.0
+const JUMP_VELOCITY : float = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody3D nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
