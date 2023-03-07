e125781cb0220dc46bda6f7d0574a44f1269c58a
Tareq Anuar <MTareqAzim@gmail.com>
2023-03-07 17:23:02 +0800

    Refactor exit for Android.

--- scripts/exit_scene.gd
+++ scripts/exit_scene.gd
@@ -2,5 +2,9 @@ extends Node
 
 
 func _ready():
-	get_tree().get_root().propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	if OS.get_name() == "Android":
+		get_tree().get_root().propagate_notification(NOTIFICATION_WM_GO_BACK_REQUEST)
+	else:
+		get_tree().get_root().propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	
 	get_tree().quit()
