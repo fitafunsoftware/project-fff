136ef86187f048511ae0a72d1609406efcca55a2
Tareq Anuar <MTareqAzim@gmail.com>
2023-03-10 00:05:49 +0800

    Create a next_scene_manager.

--- utilities/game_viewport/game_viewport.gd
+++ utilities/game_viewport/game_viewport.gd
@@ -3,6 +3,12 @@ class_name GameViewport
 
 var _loading_scene : Resource = preload("res://utilities/loading_screen/loading_screen.tscn")
 
+@onready var next_scene_manager = $NextSceneManager
+
+
+func _ready():
+	next_scene_manager.next_scene_ready.connect(_on_next_scene_ready)
+
 
 func queue_scene(path: String) -> void:
 	ResourceQueue.queue_resource(path)
@@ -15,17 +21,21 @@ func change_scene(path: String) -> void:
 		return
 	
 	var new_scene = new_scene_resource.instantiate()
-	change_scene_node(new_scene)
+	ready_scene_node(new_scene)
 
 
-func change_scene_node(next_scene: Node):
-	if get_child_count() > 0:
-		get_child(0).queue_free()
-	add_child(next_scene)
+func ready_scene_node(next_scene: Node):
+	next_scene_manager.ready_next_scene(next_scene)
 
 
 func change_to_loading_scene(path: String) -> void:
 	var loading_screen = _loading_scene.instantiate()
 	loading_screen.next_scene = path
 	
-	change_scene_node(loading_screen)
+	ready_scene_node(loading_screen)
+
+
+func _on_next_scene_ready(next_scene: Node):
+	for child in get_children():
+		if not child in [next_scene, next_scene_manager]:
+			child.queue_free()
