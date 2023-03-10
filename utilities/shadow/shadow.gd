7c73ae86278c7db7016b39c5b1511687530a2cc6
Tareq Anuar <MTareqAzim@gmail.com>
2023-03-10 15:31:01 +0800

    Set up lights properly. Made them their own scene.

--- utilities/shadow/shadow.gd
+++ utilities/shadow/shadow.gd
@@ -4,9 +4,7 @@ class_name Shadow3D
 
 
 func _init():
-	axis = Vector3.AXIS_Y
 	double_sided = false
 	alpha_cut = SpriteBase3D.ALPHA_CUT_DISCARD
 	texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
 	cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_SHADOWS_ONLY
-
