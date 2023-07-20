1e4d9f292ff1d59f47e76b59ed6e83c8fc4e7f28
Tareq Anuar <MTareqAzim@gmail.com>
2023-07-20 21:04:02 +0800

    Add dev button to control dev tools.

--- addons/debug_menu/debug_menu.gd
+++ addons/debug_menu/debug_menu.gd
@@ -127,11 +127,7 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed(&"cycle_debug_menu"):
-	if event.is_action_pressed("leftshoulder"):
-		style = wrapi(style - 1, 0, Style.MAX) as Style
-	
-	if event.is_action_pressed("rightshoulder"):
+	if event.is_action_pressed("devbutton"):
 		style = wrapi(style + 1, 0, Style.MAX) as Style
 
 
