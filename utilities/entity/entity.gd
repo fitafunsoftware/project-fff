632ea7468fb7060538847a97bb2bef8a1e520cbb
Tareq Anuar <MTareqAzim@gmail.com>
2023-07-22 21:43:22 +0800

    Create the Entity class.

---
+++ utilities/entity/entity.gd
@@ -0,0 +1,103 @@
+extends CharacterBody3D
+class_name Entity
+
+@export var max_speed : float = 10.0 :
+	get:
+		return max_speed
+	set(value):
+		max_speed = value
+
+@export var friction : float = 4.0 :
+	get:
+		return friction
+	set(value):
+		friction = value
+
+@export var acceleration : float = 4.0 :
+	get:
+		return acceleration
+	set(value):
+		acceleration = value
+
+@onready var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity", 9.8)
+
+var target_velocity_2d := Vector2.ZERO :
+	get:
+		return target_velocity_2d
+	set(value):
+		target_velocity_2d = value
+
+
+func  _init():
+	floor_constant_speed = true
+	platform_on_leave = CharacterBody3D.PLATFORM_ON_LEAVE_ADD_UPWARD_VELOCITY
+
+
+func _physics_process(delta):
+	velocity = _apply_gravity(velocity, delta)
+	velocity = _apply_acceleration(velocity, target_velocity_2d, delta)
+	velocity = _clamp_velocity(velocity)
+	
+	var _collided = move_and_slide()
+	
+	velocity = _zero_out_velocity(velocity)
+
+
+func _apply_gravity(_velocity: Vector3, delta: float) -> Vector3:
+	_velocity.y -= gravity * delta
+	return _velocity
+
+
+func _apply_acceleration(_velocity: Vector3, _target_velocity_2d: Vector2, delta: float) -> Vector3:
+	var velocity_2d := Vector2(_velocity.x, _velocity.z)
+	var final_acceleration := 0.0
+	
+	if target_velocity_2d == Vector2.ZERO:
+		final_acceleration = friction
+	else:
+		if velocity_2d.is_equal_approx(Vector2.ZERO):
+			final_acceleration = acceleration
+		elif signf(target_velocity_2d.x) != signf(velocity_2d.x) \
+				or signf(target_velocity_2d.y) != signf(velocity_2d.y):
+			final_acceleration = friction + acceleration
+		else:
+			final_acceleration = acceleration
+	
+	var final_velocity_2d = velocity_2d.move_toward(target_velocity_2d, final_acceleration * delta)
+	var final_y_velocity = _velocity.y
+	
+	var final_velocity = Vector3(final_velocity_2d.x, final_y_velocity, final_velocity_2d.z)
+	return final_velocity
+
+
+func _clamp_velocity(_velocity: Vector3) -> Vector3:
+	_velocity.x = clampf(_velocity.x, -max_speed, max_speed)
+	_velocity.y = clampf(_velocity.y, -max_speed, max_speed)
+	_velocity.z = clampf(_velocity.z, -max_speed, max_speed)
+	
+	return _velocity
+
+ 
+func _zero_out_velocity(_velocity: Vector3) -> Vector3:
+	_velocity.x = 0.0 if is_zero_approx(_velocity.x) else _velocity.x
+	_velocity.y = 0.0 if is_zero_approx(_velocity.y) else _velocity.y
+	_velocity.z = 0.0 if is_zero_approx(_velocity.z) else _velocity.z
+	
+	return _velocity
+
+
+func get_y_velocity() -> float:
+	return velocity.y
+
+
+func set_y_velocity(y_velocity: float):
+	velocity.y = y_velocity
+
+
+func get_velocity_2d() -> Vector2:
+	return Vector2(velocity.x, velocity.z)
+
+
+func set_velocity_2d(velocity_2d: Vector2):
+	velocity = Vector3(velocity_2d.x, velocity.y, velocity_2d.y)
+
