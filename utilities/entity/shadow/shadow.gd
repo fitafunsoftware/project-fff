c13fb4c8a3efa82bd85b9d63150b44f80c24d2b7
Tareq Anuar <MTareqAzim@gmail.com>
2024-03-14 22:15:31 +0800

    Shadows scale themselves to fit the floor.

--- utilities/entity/shadow/shadow.gd
+++ utilities/entity/shadow/shadow.gd
@@ -3,7 +3,7 @@ class_name Shadow3D
 extends Sprite3D
 ## Node for Shadow Sprites.
 ##
-## I am just lazy and made this to easily set these values when creating the node.
+## I am just lazy and made this to easily set these values when creating the node
 
 
 func _init():
@@ -11,3 +11,9 @@ func _init():
 	alpha_cut = SpriteBase3D.ALPHA_CUT_DISCARD
 	texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
 	cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_SHADOWS_ONLY
+
+
+func _ready():
+	if Engine.is_editor_hint():
+		return
+	scale.z /= GlobalParams.get_global_param("FLOOR_GRADIENT")
