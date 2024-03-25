3d1762cc3525bee3dae937812090abd53fe2a200
Tareq Anuar <MTareqAzim@gmail.com>
2024-03-26 00:43:28 +0800

    Implement ParallaxBackground3D node.

--- utilities/sprite_3d/README.md
+++ utilities/sprite_3d/README.md
@@ -12,3 +12,10 @@ Meant to be used for horizontal sprites in the 3D space like the ground or roofs
 Basic Sprite3D node to be used for sprites to conform to the curved world shader.<br>
 If you set or unset the shaded variable, a corresponding shaded or unshaded shader will be used.
 
+## ParallaxBackground3D
+A basic Sprite3D node that helps integrate the ParallaxBackground node functionality in 3D. The idea is to attach a ViewportTexture with a SubViewport that has a ParallaxBackground and to draw it onto the ParallaxBackground3D node. The ParallaxBackground3D node deals with setting the ParallaxBackground offset and making sure it follows the current Camera3D.<br>
+### Caveats
+- You can use the SpriteViewport to help with setting up a SubViewport with the right size, but it's recommended to increase the size to cover more than the screen shows.
+	- Setting up mirroring helps properly repeat the background so a bigger SubViewport size shows the proper background.
+- The global X and Z positions are what influence the ParallaxBackground offset scaled to the PIXEL_SIZE set in global parameters.
+- Maintains the initial Y position set.
--- utilities/sprite_3d/parallax_sprite_3d.gd
+++ utilities/sprite_3d/parallax_sprite_3d.gd
@@ -1,38 +1,74 @@
-class_name ParallaxSprite3D
+@tool
+@icon("res://utilities/sprite_3d/ParallaxBackground3D.svg")
+class_name ParallaxBackground3D
 extends Sprite3D
-## Paralax sprite for 3D.
+## Parallax sprite for 3D.
 ##
-## Node to emulate paralax backgrounds in 3D. Not fully implemented yet.
-## @experimental
-
-@export var x_scale : float = 0.0
-@export var y_scale : float = 0.0
-@export var z_scale : float = 0.0
+## A helper node to add parallax to 3D scenes. Meant to be used with a
+## ViewportTexture and a single ParallaxBackground node to work. This helper node
+## set the offset of the ParallaxBackground node based on the 3D X and Z 
+## camera positions.
 
 static var PIXEL_SIZE : float = NAN
-@onready var _prev_position : Vector3 = global_position
-var _left_over := Vector3.ZERO
+
+var _parallax_background : ParallaxBackground
+var _current_camera_3D : Camera3D
+@onready var _global_y_position : float = global_position.y
+
+
+func _init():
+	# Sets the render priority to a lower default value just so that it
+	# gets drawn behind the regular 3D sprites in the scene.
+	# Note that render priority only affects meshes with transparency.
+	render_priority = -8
 
 
 func _ready():
+	if texture:
+		_on_texture_changed()
+		texture.changed.connect(_on_texture_changed)
+	
+	if Engine.is_editor_hint():
+		set_process(false)
+		return
+	
 	if is_nan(PIXEL_SIZE):
 		PIXEL_SIZE = GlobalParams.get_global_param("PIXEL_SIZE")
 
 
-func _init():
-	cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-
-
 func _process(_delta):
-	var displacement : Vector3 = _prev_position - global_position
-	var scaled_displacement : Vector3 = displacement \
-			* Vector3(x_scale, y_scale, z_scale)
-	var total_displacement : Vector3 = scaled_displacement + _left_over
-	var snapped_displacement : Vector3 = snapped(total_displacement, 
-			Vector3(PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+	if not _current_camera_3D or not _current_camera_3D.current:
+		_attach_to_camera()
 	
-	position += Vector3(snapped_displacement.x, 
-			snapped_displacement.y + snapped_displacement.z, 0.0)
+	if not _parallax_background:
+		return
 	
-	_prev_position = global_position
-	_left_over = total_displacement - snapped_displacement
+	_parallax_background.scroll_offset = Vector2(
+			_current_camera_3D.global_position.x/PIXEL_SIZE,
+			_current_camera_3D.global_position.z/PIXEL_SIZE)
+	
+	# Set the y position so the sprite stays at the same relative y position.
+	global_position.y = _global_y_position
+
+
+# Attach to the current camera to let the camera deal with movement.
+func _attach_to_camera():
+	_current_camera_3D = get_viewport().get_camera_3d()
+	
+	if get_parent():
+		get_parent().remove_child(self)
+	
+	_current_camera_3D.add_child(self)
+	position.z = -_current_camera_3D.far + 0.01
+
+
+func _on_texture_changed():
+	if not texture is ViewportTexture:
+		return
+	
+	var parallax_backgrounds : Array = \
+			find_children("*", "ParallaxBackground", true, false)
+	if parallax_backgrounds.is_empty():
+		return
+	_parallax_background = parallax_backgrounds[0]
+
