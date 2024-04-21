cc1db17c3577212c712a36330115c3750bd67ea2
Tareq Anuar <MTareqAzim@gmail.com>
2024-04-22 00:52:46 +0800

    Snap player input to 8 directions.

--- utilities/entity/input_handlers/player_input_handler.gd
+++ utilities/entity/input_handlers/player_input_handler.gd
@@ -18,4 +18,11 @@ func _unhandled_input(event):
 
 
 func get_input_direction() -> Vector2:
-	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_direction : Vector2 = \
+			Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var angle : float = rad_to_deg(input_direction.angle())
+	input_direction.x = int(absf(angle) < 67.5) - int(absf(angle) > 112.5) \
+			if not is_zero_approx(input_direction.length_squared()) else 0
+	input_direction.y = int(signf(angle)) \
+			* int(absf(angle) > 22.5 and absf(angle) < 157.5)
+	return input_direction.normalized()
