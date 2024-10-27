f7b656066f13bb3aebfa8e64e3475fc51b791a8c
Tareq Anuar <mtareqazim@gmail.com>
2024-10-27 10:07:06 +0800

    Clean up signatures.

--- entity_spawner.gd
+++ entity_spawner.gd
@@ -2,5 +2,15 @@ class_name EntitySpawner
 extends Node3D
 
 
-func spawn_entity(entity : String, arguments : Dictionary, multiplayer_id : int):
+func spawn_entity(entity: String, arguments: Dictionary):
+	if multiplayer.is_server():
+		# Server side spawning.
+		pass
+	else:
+		# Client side spawning.
+		pass
+
+
+func sync_entity(entity_id: int):
+	# Syncing entity that was already spawned previously.
 	pass
