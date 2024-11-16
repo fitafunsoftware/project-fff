9a88994df10a0c0dbd1770b4b766bc767b00ae32
Tareq Anuar <mtareqazim@gmail.com>
2024-11-16 14:28:34 +0900

    Create EntitySpawnRequest and EntityFunctionCall resources.

---
+++ entity_spawn_request.gd
@@ -0,0 +1,17 @@
+class_name EntitySpawnRequest
+extends Resource
+
+var entity_path: String
+var pre_add_functions: Array[EntityFunctionCall]
+var post_add_functions: Array[EntityFunctionCall]
+var deferred_functions: Array[EntityFunctionCall]
+
+
+func _init(entity_path: String,
+		pre_add_functions: Array[EntityFunctionCall] = Array(),
+		post_add_functions: Array[EntityFunctionCall] = Array(),
+		deferred_functions: Array[EntityFunctionCall] = Array()):
+	self.entity_path = entity_path
+	self.pre_add_functions = pre_add_functions
+	self.post_add_functions = post_add_functions
+	self.deferred_functions = deferred_functions
