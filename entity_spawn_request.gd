1212221675e412b7637eed38d62f4b753232305d
Tareq Anuar <mtareqazim@gmail.com>
2024-11-25 13:03:24 +0900

    Statically type Dictionaries.

--- entity_spawn_request.gd
+++ entity_spawn_request.gd
@@ -7,9 +7,9 @@ var deferred_functions: Array[EntityFunctionCall]
 
 
 func _init(entity_path: String,
-		pre_add_functions: Array[EntityFunctionCall] = Array(),
-		post_add_functions: Array[EntityFunctionCall] = Array(),
-		deferred_functions: Array[EntityFunctionCall] = Array()):
+		pre_add_functions: Array[EntityFunctionCall],
+		post_add_functions: Array[EntityFunctionCall],
+		deferred_functions: Array[EntityFunctionCall]):
 	self.entity_path = entity_path
 	self.pre_add_functions = pre_add_functions
 	self.post_add_functions = post_add_functions
