0af38f2b4aab5d16da6297c3aac3ab4ce98d4020
Tareq Anuar <mtareqazim@gmail.com>
2025-01-29 22:50:07 +0800

    Add more general functions.

--- blackboard.gd
+++ blackboard.gd
@@ -13,8 +13,24 @@ func get_entities_in_level(level_id: int) -> Array[int]:
 	return _level.get_entities_in_level(level_id)
 
 
+func add_entity_to_level(level_id: int, entity_id: int):
+	_level.add_entity_to_level(level_id, entity_id)
+
+
+func erase_entity_from_level(level_id: int, entity_id: int):
+	_level.erase_entity_from_level(level_id, entity_id)
+
+
 func get_entity_spawn_request(entity_id: int) -> EntitySpawnRequest:
 	return _entity.get_entity_spawn_request(entity_id)
+
+
+func add_entity_spawn_request(entity_id: int, spawn_request: EntitySpawnRequest):
+	_entity.add_entity_spawn_request(entity_id, spawn_request)
+
+
+func erase_entity(entity_id: int):
+	_entity.erase_entity(entity_id)
 
 
 func _append_blackboards():
