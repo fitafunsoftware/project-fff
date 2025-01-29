6a0e2138a140e6158ac6fcc7772bd151a05432a7
Tareq Anuar <mtareqazim@gmail.com>
2025-01-29 21:49:53 +0800

    Add general functions.

--- entity_blackboard.gd
+++ entity_blackboard.gd
@@ -1,3 +1,23 @@
 class_name EntityBlackboard
 extends Node
 
+var _spawn_requests: Dictionary[int, EntitySpawnRequest]
+
+
+func get_entity_spawn_request(entity_id: int) -> EntitySpawnRequest:
+	if _spawn_requests.has(entity_id):
+		return _spawn_requests[entity_id]
+	
+	return null
+
+
+func add_entity_spawn_request(entity_id: int, spawn_request: EntitySpawnRequest):
+	_spawn_requests[entity_id] = spawn_request
+
+
+func erase_entity(entity_id: int):
+	_spawn_requests.erase(entity_id)
+
+
+func entity_exists(entity_id: int) -> bool:
+	return _spawn_requests.has(entity_id)
