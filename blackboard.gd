2827cd6209aaae107ef4e4a381596ee3df512f71
Tareq Anuar <mtareqazim@gmail.com>
2025-02-02 18:36:21 +0800

    Add remaining LevelBlackboard functions to Blackboard.

--- blackboard.gd
+++ blackboard.gd
@@ -21,8 +21,16 @@ func add_entity_to_level(level_id: int, entity_id: int):
 	_level.add_entity_to_level(level_id, entity_id)
 
 
-func erase_entity_from_level(level_id: int, entity_id: int):
-	_level.erase_entity_from_level(level_id, entity_id)
+func add_entities_to_level(level_id: int, entity_ids: Array[int]):
+	_level.add_entities_to_level(level_id, entity_ids)
+
+
+func erase_level(level_id: int):
+	_level.erase_level(level_id)
+
+
+func remove_entity_from_level(level_id: int, entity_id: int):
+	_level.remove_entity_from_level(level_id, entity_id)
 
 
 # EntityBlackboard
@@ -34,10 +42,20 @@ func add_entity_spawn_request(entity_id: int, spawn_request: EntitySpawnRequest)
 	_entity.add_entity_spawn_request(entity_id, spawn_request)
 
 
+# Mixed functions.
 func erase_entity(entity_id: int):
+	_level.erase_entity(entity_id)
 	_entity.erase_entity(entity_id)
 
 
+func erase_level_and_entities(level_id: int):
+	var entity_ids: Array[int] = _level.get_entities_in_level(level_id)
+	for entity_id: int in entity_ids:
+		_entity.erase_entity(entity_id)
+	_level.erase_level_and_entities(level_id)
+
+
+# Helper functions.
 func _append_blackboards():
 	_level = LevelBlackboard.new()
 	_entity = EntityBlackboard.new()
