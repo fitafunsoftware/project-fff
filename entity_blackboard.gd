de20a085bdea8961c381041b9aa9e7cafcbb816f
Tareq Anuar <mtareqazim@gmail.com>
2025-02-02 18:40:07 +0800

    Add convenience function to EntityBlackboard.

--- entity_blackboard.gd
+++ entity_blackboard.gd
@@ -16,8 +16,18 @@ func add_entity_spawn_request(entity_id: int, spawn_request: EntitySpawnRequest)
 
 
 func erase_entity(entity_id: int):
-	_spawn_requests.erase(entity_id)
+	_erase_entity(entity_id)
+
+
+func erase_entities(entity_ids: Array[int]):
+	for entity_id: int in entity_ids:
+		_erase_entity(entity_id)
 
 
 func entity_exists(entity_id: int) -> bool:
 	return _spawn_requests.has(entity_id)
+
+
+# Helper functions.
+func _erase_entity(entity_id):
+	_spawn_requests.erase(entity_id)
