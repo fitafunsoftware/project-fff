acc1f829fd16c710850bc808703da8aafabfdb27
Tareq Anuar <mtareqazim@gmail.com>
2025-02-02 18:36:21 +0800

    Add remaining LevelBlackboard functions to Blackboard.

--- level_blackboard.gd
+++ level_blackboard.gd
@@ -7,18 +7,18 @@ var _level_entities: Dictionary[int, Array]
 var _entities_level: Dictionary[int, int]
 
 
-func get_entities_in_level(level_id: int) -> Array[int]:
-	if _level_entities.has(level_id):
-		return _level_entities[level_id] as Array[int]
-	
-	return Array() as Array[int]
-
-
 func get_entity_level(entity_id: int) -> int:
 	if not _entities_level.has(entity_id):
 		return PLACEHOLDER_LEVEL_ID
 	
 	return _entities_level[entity_id]
+
+
+func get_entities_in_level(level_id: int) -> Array[int]:
+	if _level_entities.has(level_id):
+		return _level_entities[level_id] as Array[int]
+	
+	return Array() as Array[int]
 
 
 func add_entity_to_level(level_id: int, entity_id: int):
@@ -54,7 +54,7 @@ func erase_level_and_entities(level_id: int):
 	_level_entities.erase(level_id)
 
 
-func erase_entity_from_level(level_id: int, entity_id: int):
+func remove_entity_from_level(level_id: int, entity_id: int):
 	if _level_entities.has(level_id):
 		_erase_entity_from_current_level(entity_id)
 		_entities_level[entity_id] = PLACEHOLDER_LEVEL_ID
