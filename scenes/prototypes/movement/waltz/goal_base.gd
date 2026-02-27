ca23439842b055201d01551f84f7adff53e90cbb
Tareq Anuar <mtareqazim@gmail.com>
2026-02-27 08:18:05 +0800

    Simplify scripts.

--- scenes/prototypes/movement/waltz/goal_base.gd
+++ scenes/prototypes/movement/waltz/goal_base.gd
@@ -15,6 +15,10 @@ const MAX_RANGE: int = 320
 @export var base_speed: int = 100
 @export var sprint_ratio: float = 1.5
 
+@export_category("Movement Logic")
+@export var max_rest_duration: float = 2.0
+@export var sprint_probability: float = 0.2
+
 var direction: int = 0
 var is_sprinting: bool = false
 
@@ -47,7 +51,30 @@ func _physics_process(delta: float) -> void:
 
 
 func start_move() -> void:
-	pass
+	_next_move()
+
+
+func _next_move() -> void:
+	var next_position: int = _get_new_position()
+	var goal_position: int = int(position.x)
+	var distance: int = abs(next_position - goal_position)
+	var duration: float = float(distance) / base_speed
+	
+	direction = signi(next_position - goal_position)
+	is_sprinting = randf() < sprint_probability
+	await get_tree().create_timer(duration).timeout
+	
+	var rest_duration: float = \
+			clampf(randfn(max_rest_duration*3.0/4.0, 1.0), 0.0, max_rest_duration)
+	direction = 0
+	is_sprinting = false
+	await get_tree().create_timer(rest_duration).timeout
+	
+	_next_move()
+
+
+func _get_new_position() -> int:
+	return (randi() % (MAX_RANGE*2)) - MAX_RANGE
 
 
 func _move(delta: float) -> void:
